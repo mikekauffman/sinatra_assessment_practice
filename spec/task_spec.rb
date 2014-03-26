a8bb57ef8a9a790ad8543955ed305b5341aa2066
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-26 16:00:31 -0600

    homepage displays welcome and a link to create a new task

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -6,9 +6,10 @@ Capybara.app = App
 
 feature 'Access and manage homepage' do
 
-  scenario 'hompage displays welcome' do
+  scenario 'hompage displays welcome and link to create a new task' do
   visit '/'
     expect(page).to have_content "Welcome"
+    click_link "Create Task"
   end
 
 end

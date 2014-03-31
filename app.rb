ef7522fee27238107eedb7ce817c73b20fcc2364
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-30 22:47:28 -0600

    user visit a homepage, clicks add a task, enters a task, and sees that task on the homepage alongside Welcome

--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+
+  TASKLIST = []
+
   get '/' do
-    'Welcome'
+    erb :index, :locals => {:tasks => TASKLIST}
   end
+
+  get '/tasks/new' do
+    erb :add_task
+  end
+
+  post '/' do
+    TASKLIST << params[:create_task]
+    redirect '/'
+  end
+
 end
--- spec/homepage_spec.rb
+++ spec/homepage_spec.rb
@@ -5,8 +5,13 @@ require 'spec_helper'
 Capybara.app = App
 
 feature 'User visits the homepage' do
-  scenario 'Welcome displays on the homepage' do
+  scenario 'User clicks a link Add a Task' do
     visit '/'
     expect(page).to have_content 'Welcome'
+    click_link 'Add a Task'
+    fill_in 'create_task', :with => 'Exercise'
+    click_on 'Create Task'
+    expect(page).to have_content 'Welcome'
+    expect(page).to have_content 'Exercise'
   end
 end
